98acfea526ae84f1553c457089d497d5dc997e7d
solia <solomialenio13@gmail.com>
2019-03-09 16:54:27 +0200

    Initial commit

---
+++ examples/api_usage.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+
+#to do:
+def format_html():
+    pass
+
+
+link = 'http://data.rada.gov.ua/ogd/zal/agenda/skl8/sten/20141127.htm'
+
+page_response = requests.get(link, timeout=5)
+page_content = BeautifulSoup(page_response.content, "html.parser")
+
+textContent = []
+for i in range(0, len(page_content.find_all("p", attrs={"align": None}))):
+   paragraphs = page_content.find_all("p", attrs={"align": None})[i].text
+   textContent.append(paragraphs)
+print(textContent)
